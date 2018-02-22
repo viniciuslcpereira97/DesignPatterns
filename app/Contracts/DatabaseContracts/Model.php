a5bad6506141fdcca45b120c2603f7df01710337
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-22 12:05:55 -0300

    Model interface

---
+++ app/Contracts/DatabaseContracts/Model.php
@@ -0,0 +1,26 @@
+***REMOVED***
+
+namespace PHPatterns\Contracts\DatabaseContracts;
+
+interface Model
+***REMOVED***
+    /**
+     * Find ocurrences at table where id
+     * @var Int
+     * @return (Array|Object)
+     */
+    public static function find(Int $id);
+
+    /**
+     * Find all ocurrences at table
+     * @return (Array|Object)
+     */
+    public static function all();
+
+    /**
+     * Create new ocurrence at table
+     * @var Array
+     */
+    public static function create(Array $attr);
+
+***REMOVED***
