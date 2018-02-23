6727da26db7b51261a47b755251744817382697a
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-23 15:04:33 -0300

    _get implemented and try to connect to database

--- app/Database/DatabaseConnection.php
+++ app/Database/DatabaseConnection.php
@@ -8,9 +8,27 @@ class DatabaseConnection extends \PHPatterns\Singleton\Singleton
 {
     private $connection;
 
+    /**
+     *
+     * Creates PDO mysql database connection
+     * Handles if PDO Exception was trown
+     *
+     */
     protected function __construct() {
-        echo "construct";
-        $this->connection = new PDO('mysql:host=localhost;dbname=php_patterns_test', 'root', 'shambala');
+        try {
+            $this->connection = new PDO('mysql:host=localhost;dbname=php_patterns_test', 'root', 'shambala');
+        } catch(PDOException $e) {
+            echo "ERROR: {$e}";
+        }
+    }
+
+    /**
+     *
+     * Retrieves PDO connection
+     *
+     */
+    public function __get($name) {
+        return $this->$name;
     }
 
 }
