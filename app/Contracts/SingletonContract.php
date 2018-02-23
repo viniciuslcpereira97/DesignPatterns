2cc0b4cc8d0f2f41ed31f65d57e27ea3556c1a76
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-23 11:55:09 -0300

    Singleton Contract

---
+++ app/Contracts/SingletonContract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPatterns\Contracts;
+
+interface SingletonContract
+{
+
+    /**
+     *
+     * Returns instance of Singleton Object
+     *
+     */
+    public static function getInstance();
+
+}
