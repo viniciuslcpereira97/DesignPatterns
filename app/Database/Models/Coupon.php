46544a1e0429bcddbbbaba54bb88f0839cfba6d1
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-23 17:08:00 -0300

    Some commentaries

--- app/Database/Models/Coupon.php
+++ app/Database/Models/Coupon.php
@@ -7,12 +7,22 @@ use PHPatterns\Database\Models\Model;
 class Coupon extends Model
 {
 
+    /**
+     *
+     * Generates new Coupon
+     *
+     */
     public static function generate() {
         $instance = new static;
         $instance->hash = $instance->generateHash();
         return $instance;
     }
 
+    /**
+     *
+     * Generates Coupon hash
+     *
+     */
     private function generateHash() {
         $b64 = base64_encode(rand(111111111111, 999999999999));
         return hash('sha256', $b64, false);
