e4781e44536e84b71bab6b0b19d14d8cbcdd327b
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-23 17:09:14 -0300

    User Facade created

---
+++ app/Facades/UserFacade.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PHPatterns\Facades;
+
+use PHPatterns\Database\Models\User;
+use PHPatterns\Database\Models\Coupon;
+
+class UserFacade
+{
+
+    /**
+     *
+     * Creates new User and attaches a Coupon for him
+     *
+     */
+    public static function newUserWithCoupon($userAttr = []) {
+        $user = new User;
+        if(isset($userAttr) and $userAttr !== [])
+            $user->setNickname($userAttr['nickname']);
+        $coupon = Coupon::generate();
+        var_dump($user, $coupon);
+    }
+
+}
