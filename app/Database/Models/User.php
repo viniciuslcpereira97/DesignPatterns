020d74371c46afe1180dfdeed79b3b59fae6bb16
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-02-23 17:09:00 -0300

    attach coupon and nickname for user

--- app/Database/Models/User.php
+++ app/Database/Models/User.php
@@ -5,7 +5,29 @@ namespace PHPatterns\Database\Models;
 use PHPatterns\Traits\ModelsTrait;
 use PHPatterns\Database\Models\Model;
 
+use PHPatterns\Database\Models\Coupon;
+
 class User extends Model
 {
+
+    protected $table;
+
+    /**
+     *
+     * Set user nickname
+     *
+     */
+    public function setNickname($nickname) {
+        $this->nickname = $nickname;
+    }
+
+    /**
+     *
+     * Adds coupon for user
+     *
+     */
+    public function addCoupon(Coupon $coupon) {
+        $this->coupon = $coupon;
+    }
 
 }
